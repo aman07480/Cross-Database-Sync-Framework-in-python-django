4e145559642c4137c8b660ba8e32c4c4676694e9
aman tamboli 07 <tamboliaman109@gmail.com>
2026-02-19 23:01:46 +0530

    updatd views file

--- views.py
+++ views.py
@@ -1,5 +1,29 @@
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from .models import Product
+
+
+# class CreateProductView(APIView):
+
+#     def post(self, request):
+#         name = request.data.get("name")
+#         price = request.data.get("price")
+
+#         product = Product.objects.create(
+#             name=name,
+#             price=price
+#         )
+
+#         return Response({
+#             "msg": "Product created & synced",
+#             "id": product.id
+#             "no":product.no
+#         })
+
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Product
 
 
@@ -14,8 +38,13 @@ class CreateProductView(APIView):
             price=price
         )
 
-        return Response({
-            "msg": "Product created & synced",
-            "id": product.id
-            "no":product.no
-        })
+        return Response(
+            {
+                "msg": "Product created & synced",
+                "data": {
+                    "id": product.id,
+                    "no": product.no
+                }
+            },
+            status=status.HTTP_201_CREATED
+        )
