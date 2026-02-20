98f0c1969e23cbfa0391088e5dcaf62ef1cfac06
aman tamboli 07 <tamboliaman109@gmail.com>
2026-02-20 12:00:03 +0530

    update signals file nd code

--- signal.py
+++ signal.py
@@ -1,4 +1,14 @@
-from django.db.models.signals import post_save
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+# from .models import Product
+# from .sync_engine import sync_to_replica
+
+
+# @receiver(post_save, sender=Product)
+# def auto_sync_product(sender, instance, **kwargs):
+#     sync_to_replica(instance)
+
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import Product
 from .sync_engine import sync_to_replica
@@ -7,3 +17,8 @@ from .sync_engine import sync_to_replica
 @receiver(post_save, sender=Product)
 def auto_sync_product(sender, instance, **kwargs):
     sync_to_replica(instance)
+
+
+@receiver(post_delete, sender=Product)
+def auto_delete_replica(sender, instance, **kwargs):
+    Product.objects.using('replica').filter(id=instance.id).delete()
