5f43275393cb530abf6605130b7dfb6e377a8c6a
aman tamboli 07 <tamboliaman109@gmail.com>
2026-02-21 19:44:14 +0530

    changes in urls file

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreateProductView
+from .views import CreateProductView,ManualResyncView
 
 urlpatterns = [
     path("create-product/", CreateProductView.as_view()),
+    path("resync/<int:pk>/", ManualResyncView.as_view()),
 ]
