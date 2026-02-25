ad7c2610e181985e84cfac0cde8a002ee5103341
aman tamboli 07 <tamboliaman109@gmail.com>
2026-02-25 22:57:20 +0530

    changes in models file

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     updated_at = models.DateTimeField(auto_now=True)
+    sync_status = models.CharField(max_length=20, default="pending")
 
     def __str__(self):
         return self.name
